dea0c2b41d22f1c36f2afbcf8a59429b5a1fe480
Gabriel Nowak <gbrl.nowak@gmail.com>
2018-01-18 13:23:07 +0100

    Update euler1.cpp

--- euler1.cpp
+++ euler1.cpp
@@ -5,13 +5,13 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #include<iostream>
 using namespace std;
 
-int sumOfMultiples(int x, int maximum)
+int sumOfMultiples(int x, int maximum) //finding sum of multiples of number given by parameter x below maximum
 {
     int sum=0;
     for(int i=x;i<maximum;i+=x) sum+=i;
     return sum;
 }
-int sumOfBoth(int x, int y, int maximum)
+int sumOfBoth(int x, int y, int maximum) // finding sum of multiples which are at the same time multiples of x and y below maximum
 {
     int sum=0;
     if(y<x) swap(x,y);
@@ -25,7 +25,7 @@ int sumOfBoth(int x, int y, int maximum)
 int main()
 {
     int sum;
-    sum=sumOfMultiples(3,1000)+sumOfMultiples(5,1000)-sumOfBoth(3,5,1000);
+    sum=sumOfMultiples(3,1000)+sumOfMultiples(5,1000)-sumOfBoth(3,5,1000);// A+B-(A and B) because (A and B) is counted twice
     cout<<sum;
     return 0;
 }
